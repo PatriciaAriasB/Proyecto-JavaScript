9f4e2d0b94ca843b5148d7b00bd5bb887c22f32d
Patricia 607413324 <patriciapatriciaariasburdeos@gmail.com>
2024-04-18 21:39:11 +0200

    AñadiendoREADME.md

---
+++ README.md
@@ -0,0 +1,36 @@
+Proyecto Quiz:
+
+Este es un proyecto de juego de quiz implementado en HTML, CSS y JavaScript. El juego obtiene preguntas de la API de QuizAPI y permite a los usuarios responderlas. La puntuación del usuario se calcula en función de las respuestas correctas, y la puntuación final se muestra al final del quiz.
+Características
+
+    Obtiene preguntas de la API de QuizAPI.io.
+    Permite a los usuarios seleccionar respuestas a preguntas de opción múltiple.
+    Calcula la puntuación del usuario en función de las respuestas correctas.
+    Muestra la puntuación final al final del quiz.
+
+Instalación
+
+    Clona este repositorio en tu máquina local usando git clone.
+    Abre el archivo index.html en tu navegador web para comenzar el quiz.
+
+Uso
+
+    Ingresa tu nombre de usuario en el campo de entrada proporcionado y haz clic en "Comenzar preguntas".
+    Responde las preguntas haciendo clic en las opciones proporcionadas.
+    Continúa respondiendo preguntas hasta el final del quiz.
+    Visualiza tu puntuación final al final del quiz.
+
+Tecnologías Utilizadas
+
+    HTML
+    CSS (Bootstrap)
+    JavaScript
+    Axios (para realizar solicitudes HTTP)
+
+Créditos
+
+    QuizAPI.io: Utilizado para obtener preguntas de quiz.
+
+Autora
+
+Patricia Arias Burdeos.
--- main.js
+++ main.js
@@ -15,11 +15,13 @@ let score = 0;
 let currentQuestion = 0;
 let questions = [];
 
+
+
 const getInfo = async () => {
     try {
         const response = await axios.get(API_URL);
         questions = response.data;
-       
+
     } catch (error) {
         console.error(error);
     }
@@ -45,50 +47,50 @@ const showQuestion = () => {
         if (value !== null) {
             const button = document.createElement("button");
 
-        //     button.classList.add("badge"); 
-        //     button.classList.add("rounded-pill"); 
-        //    button.classList.add("bg-warning"); 
+            //     button.classList.add("badge"); 
+            //     button.classList.add("rounded-pill"); 
+            //    button.classList.add("bg-warning"); 
 
-
-            button.classList.add ()
+            button.classList.add()
             button.innerText = value;
             button.addEventListener("click", () => {
                 selectAnswer(key, button);
                 if (button.dataset.correctAnswer === "true") {
-                
+
                 }
             });
             answerButtonsElement.appendChild(button);
         }
-    }); 
+    });
 
     questionElement.appendChild(answerButtonsElement);
 };
 
 const selectAnswer = (selectedAnswerKey, button) => {
-  
-    
+
     const question = questions[currentQuestion];
     const correctAnswerKey = Object.keys(question.correct_answers).find(key => question.correct_answers[key] === "true").substring(0, 8);
-    
+
     Array.from(answerButtonsElement.children).forEach(button => {
-        // button.classList.remove("grey");
+
         if (button.innerText === question.answers[correctAnswerKey]) {
             button.classList.add("correct");
-            button.setAttribute("correct","correct");
+            button.setAttribute("correct", "correct");
+
         } else if (button.innerText === question.answers[selectedAnswerKey]) {
             button.classList.add("incorrect");
-            
+
         }
         button.disabled = true;
     });
-    
-    if (button.getAttribute("correct")=="correct") {
+
+    if (button.getAttribute("correct") == "correct") {
         score++;
-        scorePoints.innerHTML =`<p>Points: ${score}</p>` ;
-     
+        scorePoints.innerHTML = "";
+
+
     }
-    
+
     if (questions.length > currentQuestion + 1) {
         nextButton.classList.remove('d-none');
     } else {
@@ -98,7 +100,6 @@ const selectAnswer = (selectedAnswerKey, button) => {
         resultsDiv.classList.remove('d-none');
         result.innerHTML = `<p> ${score} POINTS <p>`;
     }
-    //nextButton.classList.remove('d-none');
 
 };
 
@@ -107,13 +108,12 @@ startButton.addEventListener('click', startQuiz);
 function setNextQuestion() {
     showQuestion(questions[currentQuestion]);
     nextButton.classList.add('d-none');
-}
 
+}
 
 nextButton.addEventListener("click", () => {
     currentQuestion++;
     setNextQuestion();
-
 
 });
 
